a2dbc3fca61c51528a72c8446fbabe7e34a5f944
Diana Lenter <dianacurca20@gmail.com>
2021-01-31 14:44:56 +0100

    Task:BankAccount

---
+++ src/main/java/fundamentals/oop/BankAccount.java
@@ -0,0 +1,134 @@
+package fundamentals.oop;
+
+/**
+ * Write an application that contains the following classes:
+ * BankAccount which is consists of:
+ * constructor: with arguments: type and currency ( for initializing the fields and the  balance will be initialized with 0 in the constructor)
+ * fields: type of the account (SAVING or SPENDING),  balance ( the amount of money), currency ( USD, EUR),
+ * methods:
+ * 1.addMoney ( adds money to the balance, - if the account is of type SPENDING the amount of money that is added needs to be bigger than 10% of balance otherwise print a suggestive  error message)
+ * 2.withdrawMoney ( withdraws  money from the balance - if the account is of type SAVING the remaining balance can not be < 0 otherwise print a suggestive  error message ),
+ * 3.covertMoneyToCurrency ( that takes 2 arrguments: amountOfMoney to convert, and the currency to convert to and returns the result = the money in the input currency)
+ */
+public class BankAccount { //ALT + Insert creiaza automat Setter si Getter
+    //creeam fildurile
+    /**
+     * Type of account:SAVING or SPENDING
+     */
+    private String type;
+    /**
+     * Balance of the account
+     */
+    private double balance;
+    /**
+     * Currency: USA or EUR
+     */
+    private String currency;  //USA /EUR
+
+    public final static double USD_TO_EUR = 0.23;  //constantele se declara cu litere mari si _
+    public final static double EUR_TO_USD = 1.23;
+
+    //creeam constructorul - nu este voie cu celasi nr de argumente
+    public BankAccount(String type, String currency) {    //1st constructor (defoult constructor)
+        this.type = type;
+        this.currency = currency;
+        this.balance = 0;
+    }
+
+    public BankAccount() {                                  //2md constructor
+        this.type = "SAVING";
+        this.currency = "USD";
+        this.balance = 0;
+    }
+
+    /**
+     * 1.addMoney ( adds money to the balance, - if the account is of type SPENDING the amount of
+     * money that is added needs to be bigger than 10% of balance otherwise print a suggestive  error message)
+     *
+     * @return
+     */
+//    public void addMoney(double newMoney) {
+//        if (this.type == "SPENDING") {
+//            if (newMoney <= this.balance / 10) {
+//                System.out.println("Please add least 10% of actual balance ");
+//            } else {
+//                this.balance += newMoney;
+//            }
+//            return;
+//        }
+//        this.balance += newMoney;
+//    }
+    //sau
+    public void addMoney1(double newMoney) {
+        if (this.type == "SPENDING" && newMoney <= this.balance / 10) {
+            System.out.println("Please add least 10% of actual balance ");
+            return;   //return te scoate din metoda complet.(iese din functie)
+        }
+        this.balance += newMoney;
+        System.out.println("Your new balance is: " + this.balance);
+    }
+
+    /**
+     * 2.withdrawMoney ( withdraws  money from the balance - if the account is of type SAVING
+     * the remaining balance can not be < 0 otherwise print a suggestive  error message ),
+     *
+     * @return
+     */
+    public void withdrawMoney(double newMoney) {
+        if (this.type == "SAVING" && this.balance - newMoney < 0) {
+            System.out.println("Your balance can't be negative! ");
+            return;
+        }
+        this.balance -= newMoney;
+        System.out.println("Your new balance is: " + this.balance);
+    }
+
+    /**
+     * 3.covertMoneyToCurrency ( that takes 2 arrguments: amountOfMoney to convert,
+     * and the currency to convert to and returns the result = the money in the input currency)
+     *
+     * @return
+     */
+
+    public static double convertMoneyToCurrency(double amountOfMuney, String inputCurrency, String outputCurrency) {
+        if (inputCurrency.equals("EUR") && outputCurrency.equals("USD")) {
+            return amountOfMuney * EUR_TO_USD;
+        }
+        return amountOfMuney * USD_TO_EUR;
+    }
+
+    @Override                       //alt+insert + toString()
+    public String toString() {
+        return "BankAccount{" +
+                "type='" + type + '\'' +
+                ", balance=" + balance +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * Return the balance of the account
+     *
+     * @return
+     */
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getCurrency() {                                  //CTRL+Click ne duce direct la metoda
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+}
---
+++ src/main/java/fundamentals/oop/Main.java
@@ -0,0 +1,43 @@
+package fundamentals.oop;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BankAccount bankAccount = new BankAccount();
+        System.out.println("Type: " + bankAccount.getType());
+        System.out.println("Currency : " + bankAccount.getCurrency());
+
+        Client client1 = new Client("Diana", "54564366774678", bankAccount);
+        System.out.println(client1.toString());
+//case 1 give bank accout to the client from outside(created outsiede)
+        client1.getBankAccount().addMoney1(12); //a returnat suma din contul respectiv
+        System.out.println(client1.toString());
+        client1.getBankAccount().setType("SPENDING");
+        client1.getBankAccount().addMoney1(1);
+        client1.getBankAccount().withdrawMoney(2);
+
+        //double x = bankAccount.convertMoneyToCurrency(10, "EUR", "USD");
+        System.out.println(bankAccount.convertMoneyToCurrency(10, "EUR", "USD"));
+        // System.out.println(x);
+
+        BankAccount bankAccount1 = new BankAccount("SAVING", "USD");    //dublam o linie de cod cu comanda ctrl+d
+        BankAccount bankAccount2 = new BankAccount("SPENDING", "USD");
+        BankAccount[] myArray = {bankAccount1, bankAccount2};
+        client1.setBankAccounts(myArray);
+        System.out.println(myArray[0]);
+        BankAccount[] clientBankAcounts = client1.getBankAccounts();
+        System.out.println(clientBankAcounts[0]);
+        System.out.println(client1.getBankAccounts()[0]);
+
+
+     /*   System.out.println("input client's name: "); //introducem de la tastatura noul client
+        String name = scanner.next();
+        System.out.println("input client's cnp: ");
+        String cnp = scanner.next();
+//case2: client has asigned by defoult a defoult bank account - create the bank account insede the constructor
+        Client client2 = new Client(name, cnp);
+        System.out.println(client2.toString());*/
+    }
+}
